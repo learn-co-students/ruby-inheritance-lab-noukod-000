28401732adf59b799ec828519bd9cdf2de1f3941
Ricartho Franck <ricartho.franck@esih.edu>
2019-04-28 15:23:08 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+class Student < User
 
+  def initialize
+    @knowledge = []
+  end 
+  
+  def learn(new_stuff)
+    @knowledge.push(new_stuff)
+  end 
+  
+  def knowledge
+    @knowledge
+  end 
 end
